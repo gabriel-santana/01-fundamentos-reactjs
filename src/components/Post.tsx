21a1ec63492489c03ee6df8454f6db4ea7269b40
gabriel-santana <gabrielsantana77@hotmail.com>
2022-08-02 21:49:02 -0300

    Post

---
+++ src/components/Post.tsx
@@ -0,0 +1,25 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+     <header>
+      <div className={styles.author}>
+        <img className={styles.avatar} src="https://github.com/diego3g.png" />
+        <div className={styles.authorInfo}>
+          <strong>Diego Fernades</strong>
+          <span>Web Developer</span>
+        </div>
+      </div>
+      <time title='7 de Julho às 08h' dateTime='2022-07-07 08:00:00'>Publicado há 1h</time>
+     </header>
+
+     <div className={styles.content}>
+      <p>Lorem Ipsum </p>
+      <p>is simply dummy text of the printing and typesetting industry.</p>
+      <p><a href='#'>typesetting industry.</a></p>
+      <p><a href='#'>#is #simply</a></p>
+     </div>
+    </article>
+  ) 
+}
